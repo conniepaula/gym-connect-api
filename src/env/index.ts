0211246b0004361ca6b22cd52f8fa5ae13fdb78d
Connie <conniedpaula@gmail.com>
2024-04-18 11:59:24 -0300

    chore: update enviromment variables to use with docker

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,16 +1,22 @@
 import 'dotenv/config'
 import { z } from 'zod'
 
-const envSchema = z.object({NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
-    PORT: z.coerce.number().default(3333)
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(['development', 'production', 'test'])
+    .default('development'),
+  PORT: z.coerce.number().default(3333),
+  POSTGRESQL_USERNAME: z.string(),
+  POSTGRESQL_PASSWORD: z.string(),
+  POSTGRESQL_DATABASE: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
 
-if (!_env.success){
-    console.error('Invalid environment variables', _env.error.format())
-    
-    throw new Error('Invalid environment variables.')
+if (!_env.success) {
+  console.error('Invalid environment variables', _env.error.format())
+
+  throw new Error('Invalid environment variables.')
 }
 
 export const env = _env.data
