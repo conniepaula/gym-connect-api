9fda71b2ebb3350073b5d6338dc506bbdfc5c713
Connie <conniedpaula@gmail.com>
2024-07-08 17:33:23 +0100

    chore: set up testing environments for e2e testing

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,35 @@
+import 'dotenv/config'
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
+import { Environment } from 'vitest'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Required environment variable DATABASE_URL not found.')
+  }
+  const url = new URL(process.env.DATABASE_URL)
+  url.searchParams.set('schema', schema)
+  return url.toString()
+}
+
+export default <Environment>{
+  name: 'prisma',
+  transformMode: 'web',
+  async setup() {
+    const schema = randomUUID()
+    const databaseURL = generateDatabaseURL(schema)
+    process.env.DATABASE_URL = databaseURL
+    execSync('pnpm dlx prisma migrate deploy')
+    return {
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect()
+      },
+    }
+  },
+}
